b975d6c07b64979a9c47bcffa7c9515ad95e0153
Sunil Kumar <sukumar@ex2india.local>
2023-06-07 20:02:22 +0530

    reporting done from Code

--- src/main/java/com/exsq/isp_plugin/base/TestBase.java
+++ src/main/java/com/exsq/isp_plugin/base/TestBase.java
@@ -1,58 +1,40 @@
 package com.exsq.isp_plugin.base;
 
-import java.awt.Desktop;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
-
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailAttachment;
-import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
-import org.apache.commons.mail.MultiPartEmail;
-import org.apache.commons.mail.SimpleEmail;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.ISuite;
-import org.testng.ISuiteResult;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
-import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.exsq.isp_plugin.pageObjects.ISP_Overview;
-
-import org.openqa.selenium.Capabilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 public class TestBase implements ITestListener{
 
@@ -62,10 +44,13 @@ public class TestBase implements ITestListener{
 	public Actions action;
 	public static ExtentReports extentReports;
 	public static ExtentTest extentTest;
-	
-	List<ITestNGMethod> passedtests = new ArrayList<ITestNGMethod>();
-	List<ITestNGMethod> failedtests = new ArrayList<ITestNGMethod>();
-	List<ITestNGMethod> skippedtests = new ArrayList<ITestNGMethod>();
+	int failedCount = 0;
+	int passedCount = 0;
+	int skippedCount = 0;
+	int totalExecutedCount = 0;
+	//List<ITestNGMethod> passedtests = new ArrayList<ITestNGMethod>();
+	//List<ITestNGMethod> failedtests = new ArrayList<ITestNGMethod>();
+	//List<ITestNGMethod> skippedtests = new ArrayList<ITestNGMethod>();
 
 
 	public WebDriverWait wait;
@@ -91,16 +76,6 @@ public class TestBase implements ITestListener{
 		extentTest=extentReports.createTest(m.getName());
 	}
 	
-	@AfterMethod
-	public void checkStatus(Method m, ITestResult result) {
-		
-		if(result.getStatus()==ITestResult.FAILURE) { 
-			extentTest.fail(result.getThrowable());
-		} else if(result.getStatus()==ITestResult.SUCCESS){
-			extentTest.pass(m.getName()+" is passed.");
-		}
-	}
-	
 	@BeforeSuite
 	public void initialiseExtentReports() {
 		ExtentSparkReporter sparkReporter=new ExtentSparkReporter("ExtentSparkReport.html");
@@ -113,13 +88,72 @@ public class TestBase implements ITestListener{
 		System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "sandbox; default-src 'self';");
 	}
 	
-	
-	
+	@AfterMethod
+	public void checkStatus(Method m, ITestResult result) {		
+		if(result.getStatus()==ITestResult.FAILURE) { 
+			extentTest.fail(result.getThrowable());
+		} else if(result.getStatus()==ITestResult.SUCCESS){
+			extentTest.pass(m.getName()+" is passed.");
+		}
+	}
+		
 	
 	@AfterSuite
-	public void generateExtentReports() throws IOException, Exception {
-		extentReports.flush();
+	public void generateExtentReports(ITestContext context) throws IOException, Exception {
+		extentReports.flush();		
+		passedCount=context.getPassedTests().size();
+		failedCount=context.getFailedTests().size();
+		skippedCount=context.getSkippedTests().size();
+		totalExecutedCount=passedCount+failedCount+skippedCount;
+		Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = sdf.format(date);
+        		
 		//Desktop.getDesktop().browse(new File("ExtentSparkReport.html").toURI());	
+		 EmailAttachment attachment = new EmailAttachment();
+		  attachment.setPath("ExtentSparkReport.html");
+		  attachment.setDisposition(EmailAttachment.ATTACHMENT);
+		  attachment.setDescription("Extent Report");
+		  //attachment.setName("Suite Test Report");
+		  
+		  // Create the email message MultiPartEmail email = new MultiPartEmail();
+		  //Email email = new SimpleEmail(); 
+		  HtmlEmail email = new HtmlEmail();
+		  email.setHostName("smtp.gmail.com");
+		  email.setSmtpPort(465); email.setAuthenticator(new
+		  DefaultAuthenticator("sukumar@ex2india.com", "dsdidbgvgwnbueac"));
+		  email.setSSLOnConnect(true); email.setFrom("sukumar@ex2india.com");
+		  email.setSubject("Test Suite Result  ("+formattedDate+")");
+		  email.addTo("sukumar@ex2india.com");
+		  // set the html message
+		  email.setHtmlMsg("<html>\r\n"
+		  		+ "	<body>\r\n"
+		  		+ "		<p>\r\n"
+		  		+ "		Hello All, <br> <br>\r\n"
+		  		+ "		\r\n"
+		  		+ "		Please find the detailed automation test result in the attachment. <br> <br>\r\n"
+		  		+ "		\r\n"
+		  		+ "		Total Executed Test Cases : "+totalExecutedCount+" <br>\r\n"
+		  		+ "		Total Passed :  "+passedCount+"<br> \r\n"
+		  		+ "		Total Failed : "+failedCount+"<br> \r\n"
+		  		+ "		Total Skipped : "+skippedCount+"<br> <br>\r\n"
+		  		+ "		\r\n"
+		  		+ "		Download the attached report to view the detailed result. <br> <br>\r\n"
+		  		+ "		\r\n"
+		  		+ "		Note : This is automated email please don't reply to this email.<br> <br>\r\n"
+		  		+ "		Thanks,<br>\r\n"
+		  		+ "		Automation Team <br>(Sunil Kumar)\r\n"
+		  		+ "		</p>\r\n"
+		  		+ "	</body>\r\n"
+		  		+ "</htm>");
+
+		  // set the alternative message
+		  email.setTextMsg("Your email client does not support HTML messages");
+		  
+		  // add the attachment 
+		  email.attach(attachment);
+		  
+		  email.send();
 	}
 	
 	@Parameters("browserName2")
@@ -179,7 +213,4 @@ public class TestBase implements ITestListener{
     		driver.navigate().to(prop.getProperty("Preview_3_0"));
     	}
     }
-    
-
-
 }
